aa4aa24d3f6b999fbea3452c06b59a774a834c9b
Nanami91 <nana.1999_04_26.ina@me.com>
2021-06-18 04:07:32 +0900

    モデルの追加

---
+++ db/migrate/20210617185216_create_noodles.rb
@@ -0,0 +1,17 @@
+class CreateNoodles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :noodles do |t|
+      t.string  :store_name,        null: false
+      t.integer :prefecture_id,     null: false
+      t.integer :price,             null: false
+      t.text    :description,       null: false
+      t.text    :menu,              null: false
+      t.text    :genre,             null: false
+      t.integer :star_id,           null: false
+      t.date    :eat_date
+      t.references :user,           null: false,   foreign_key: true
+      
+      t.timestamps
+    end
+  end
+end
