c7526fd8d1460b7f89453b43a026c1fd4f2e8df2
Nanami91 <nana.1999_04_26.ina@me.com>
2021-06-22 02:30:56 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to:"noodles#index"
   resources :noodles do
     resources :addresses, only: [:index, :new, :create, :edit, :update]
+    resources :comments, only: [:create, :destroy]
   end
   resources :users, only: [:show]
 end
